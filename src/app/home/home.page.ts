2734f8fddb7170d7f69923c30db43e31923431c7
hayyurahmayani <hayyurahmayanipuspitasari0703@mail.ugm.ac.id>
2024-09-23 14:40:21 +0700

    PGPBL Acara 6 progres

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -17,7 +17,7 @@ export class HomePage {
 
   ionViewDidEnter() {
     // Create the map
-    this.map = L.map('mapId').setView([-7.78031726531913, 110.36799547304071], 13);
+    this.map = L.map('mapId').setView([-7.78031726531913, 110.36799547304071], 11);
 
    // Basemap
    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -51,25 +51,61 @@ var basemap3 = L.tileLayer(
 );
 
 var basemap4 = L.tileLayer(
-    "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
+    'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', 
     {
-        attribution:
-            '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-    }
-);
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
+
+
+
 basemap1.addTo(this.map);
 
 var baseMaps = {
     "OpenStreetMap": basemap1,
     "Esri World Street": basemap2,
     "Esri Imagery": basemap3,
-    "Stadia Dark Mode": basemap4
+    "Open Topo Map": basemap4
 };
 
 L.control.layers(baseMaps).addTo(this.map);
 
-    // Add a marker with a popup
-    this.marker = L.marker([-7.787578605374033, 110.37422167892638]).addTo(this.map);
-    this.marker.bindPopup("<b>Sarana Olahraga</b><br>Stadion Kridosono").openPopup();
+// Marker custom dengan icon sendiri
+    const customIcon = L.icon({
+      iconUrl: 'assets/icon/marker.png', // Path ke file icon
+      iconSize: [38, 38], // Ukuran ikon
+      iconAnchor: [19, 38], // Anchor point di bawah ikon
+      popupAnchor: [0, -38], // Posisi popup relatif terhadap ikon
+    });
+
+    // Tambahkan marker dengan ikon custom
+    this.marker = L.marker([-7.7954472430867074, 110.38384628747343], { icon: customIcon }).addTo(this.map);
+    this.marker.bindPopup(`
+        <b>Stadion Mandala Krida</b>
+        <br>
+        Gedung Olahraga
+        <br>
+        <div style="text-align: center;">
+    <img src="https://storage.googleapis.com/seo-cms/assets/sejarah_stadion_mandala_krida_423a1a3f4e/sejarah_stadion_mandala_krida_423a1a3f4e.jpg" alt="Stadion Mandala Krida" width="100" height="100"></div>
+    `).openPopup();
+
+    // Marker kedua
+const marker2 = L.marker([-7.749924850440371, 110.41804480452821], { icon: customIcon }).addTo(this.map);
+marker2.bindPopup(`
+  <b>Stadion Maguwoharjo</b><br>
+  Gedung Olahraga<br>
+  <div style="text-align: center;">
+    <img src="https://yogyapos.com/images/berita/yogya-pos-stadion-maguwoharjo-segera-dibenahi-ini-agendanya-52.jpeg" alt="Stadion Maguwoharjo" width="100" height="100">
+  </div>
+`);
+
+const marker3 = L.marker([-7.875349, 110.380186], { icon: customIcon }).addTo(this.map);
+marker3.bindPopup(`
+  <b>Stadion Sultan Agung</b><br>
+  Gedung Olahraga<br>
+  <div style="text-align: center;">
+    <img src="https://static.promediateknologi.id/crop/0x0:0x0/0x0/webp/photo/p2/223/2024/04/16/stadion-sultan-agung-bantul-1885040055.jpg" alt="Stadion Sultan Agung" width="100" height="100">
+  </div>
+`);
+
   }
 }
